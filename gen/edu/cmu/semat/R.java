be1b629e24f698a845947ecb777d7bfd9c1a3245
Quentin de Metz <quentindemetz@gmail.com>
2013-11-05 21:21:28 -0800

    Add layouts for several components

--- gen/edu/cmu/semat/R.java
+++ gen/edu/cmu/semat/R.java
@@ -12,10 +12,6 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -24,28 +20,49 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int editText1=0x7f080001;
-        public static final int team_picker_list_view=0x7f080002;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f09000d;
+        public static final int checkBox1=0x7f090003;
+        public static final int content_alpha__next_button=0x7f090009;
+        public static final int content_alpha__notes_button=0x7f090008;
+        public static final int content_alpha__parent_layout=0x7f090004;
+        public static final int content_alpha__subtitle=0x7f090006;
+        public static final int content_alpha__title=0x7f090005;
+        public static final int editText1=0x7f090002;
+        public static final int gridView1=0x7f09000a;
+        public static final int listView1=0x7f090007;
+        public static final int pager=0x7f090000;
+        public static final int progressBar1=0x7f09000b;
+        public static final int team_picker_list_view=0x7f09000c;
+        public static final int textView1=0x7f090001;
+    }
+    public static final class integer {
+        /**  Integers for the index_alpha layout 
+ Integers for the index_alpha layout 
+         */
+        public static final int index_alpha__num_columns=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int alpha_current_state=0x7f030001;
-        public static final int alpha_next_actions=0x7f030002;
-        public static final int introduction=0x7f030003;
-        public static final int team_picker=0x7f030004;
+        public static final int alpha=0x7f030001;
+        public static final int alpha_current_state=0x7f030002;
+        public static final int alpha_next_actions=0x7f030003;
+        public static final int checkbox=0x7f030004;
+        public static final int content_alpha=0x7f030005;
+        public static final int index_alpha=0x7f030006;
+        public static final int introduction=0x7f030007;
+        public static final int login=0x7f030008;
+        public static final int team_picker=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int current_state=0x7f050004;
-        public static final int hello_world=0x7f050002;
-        public static final int introduction=0x7f050003;
-        public static final int next_actions=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int current_state=0x7f060004;
+        public static final int hello_world=0x7f060002;
+        public static final int introduction=0x7f060003;
+        public static final int next_actions=0x7f060005;
     }
     public static final class style {
         /** 
@@ -69,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
