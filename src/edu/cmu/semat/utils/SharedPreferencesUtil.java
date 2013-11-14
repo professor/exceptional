d816c0f7ce0d621fa2d5f54ac454c2b43071cab2
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-11-14 08:57:21 -0800

    Refactored to reduce copy and paste errors

--- src/edu/cmu/semat/utils/SharedPreferencesUtil.java
+++ src/edu/cmu/semat/utils/SharedPreferencesUtil.java
@@ -10,56 +10,90 @@ public class SharedPreferencesUtil {
 	
 	private static String preferenceFile = "edu.cmu.semat";
 	private static String splashKey = "edu.cmu.semat.splash_key";
-	private static String teamKey = "edu.cmu.semat.team_key";
+	private static String teamIdKey = "edu.cmu.semat.team_id_key";
+	private static String teamNameKey = "edu.cmu.semat.team_name_key";
 	private static String emailKey = "edu.cmu.semat.email_key";
 
-	
-	public static void setSplashScreenSeenByUser(Activity activity, boolean value) {
-		SharedPreferences prefs = activity.getSharedPreferences(
-				preferenceFile, Context.MODE_PRIVATE);
-		prefs.edit().putBoolean(splashKey, value).commit();		
-	}
-	
-	public static boolean getSplashScreenSeenByUser(Activity activity) {
-		SharedPreferences prefs = activity.getSharedPreferences(
-				preferenceFile, Context.MODE_PRIVATE);
 
-		// use a default value using new Date()
-		boolean value = prefs.getBoolean(splashKey, false);  
-		
-		return value;
-	}
-	
-	
 	public static void setCurrentTeamId(Activity activity, int teamId) {
-		SharedPreferences prefs = activity.getSharedPreferences(
-				preferenceFile, Context.MODE_PRIVATE);
-		prefs.edit().putInt(teamKey, teamId).commit();		
+		setInt(activity, teamIdKey, teamId);
 	}
 	
 	public static int getCurrentTeamId(Activity activity, int defaultTeamId) {
+		return getInt(activity, teamIdKey, defaultTeamId);
+	}	
+	
+	public static void setCurrentTeamName(Activity activity, String teamName) {
+		setString(activity, teamNameKey, teamName);
+	}
+	
+	public static String getCurrentTeamName(Activity activity, String defaultTeamName) {
+		return getString(activity, teamNameKey, defaultTeamName);
+	}		
+	
+	public static void setCurrentEmailAddress(Activity activity, String emailAddress) {
+		setString(activity, emailKey, emailAddress);
+	}
+	
+	public static String getCurrentEmailAddress(Activity activity, String defaultEmailAddress) {
+		return getString(activity, emailKey, defaultEmailAddress);
+	}	
+	
+	public static void setSplashScreenSeenByUser(Activity activity, boolean value) {
+		setBoolean(activity, splashKey, value);
+	}
+	
+	public static boolean getSplashScreenSeenByUser(Activity activity) {
+		return getBoolean(activity, splashKey, false);
+
+	}
+	
+	
+	
+	private static void setString(Activity activity, String key, String value) {
 		SharedPreferences prefs = activity.getSharedPreferences(
 				preferenceFile, Context.MODE_PRIVATE);
-
-		int value = prefs.getInt(splashKey, defaultTeamId); 
-		
+		prefs.edit().putString(key, value).commit();					
+	}
+	
+	private static String getString(Activity activity, String key, String defaultValue) {
+		SharedPreferences prefs = activity.getSharedPreferences(
+				preferenceFile, Context.MODE_PRIVATE);
+		String value = prefs.getString(key, defaultValue); 
+		return value;
+	}
+	
+	private static void setInt(Activity activity, String key, int value) {
+		SharedPreferences prefs = activity.getSharedPreferences(
+				preferenceFile, Context.MODE_PRIVATE);
+		prefs.edit().putInt(key, value).commit();					
+	}
+	
+	private static int getInt(Activity activity, String key, int defaultValue) {
+		SharedPreferences prefs = activity.getSharedPreferences(
+				preferenceFile, Context.MODE_PRIVATE);
+		int value = prefs.getInt(key, defaultValue); 
 		return value;
 	}	
 	
-	public static void setCurrentEmailAddress(Activity activity, String emailAddress) {
+	private static void setBoolean(Activity activity, String key, boolean value) {
 		SharedPreferences prefs = activity.getSharedPreferences(
 				preferenceFile, Context.MODE_PRIVATE);
-		prefs.edit().putString(emailKey, emailAddress).commit();		
+		prefs.edit().putBoolean(key, value).commit();					
 	}
 	
-	public static String getCurrentEmailAddress(Activity activity, String defaulEmailAddress) {
+	private static boolean getBoolean(Activity activity, String key, boolean defaultValue) {
 		SharedPreferences prefs = activity.getSharedPreferences(
 				preferenceFile, Context.MODE_PRIVATE);
-
-		String value = prefs.getString(emailKey, defaulEmailAddress); 
-		
+		boolean value = prefs.getBoolean(key, defaultValue);  
 		return value;
-	}
+	}		
+	
+	
+	
+	
+
+
 	
 
 }
