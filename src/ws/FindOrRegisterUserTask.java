7feb08252d3ff2a4485a52cfce27eb4b7c4e4fc2
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-12-09 10:51:59 -0800

    .

--- src/ws/FindOrRegisterUserTask.java
+++ src/ws/FindOrRegisterUserTask.java
@@ -19,45 +19,36 @@ public class FindOrRegisterUserTask extends ExceptionalTask {
 		this.email = email_address;
 	}
 
-//	
-//		public FindOrRegisterUserTask(MyApplication application,
-//			EmailPickerActivity activity, Object auth_token,
-//			String selectedEmailAddress, Object object, Object object2) {
-//		// TODO Auto-generated constructor stub
-//	}
 
-
-		@Override
-		protected String doInBackground(String... urls) {
-
-
-			Log.v(TAG, "find_or_register user " + email);
-			try {
-				return HTTPUtils.sendPost(activity, "http://semat.herokuapp.com/api/v1/users/find_or_register.json", "email=" + email);
-			} catch (IOException e) {
-				Log.v(TAG, e.getMessage());
-				return "EmailPickerActivity: Unable to retrieve web page. URL may be invalid. " + e.getMessage();
-			} catch (Exception e) {
-				Log.v(TAG, e.getMessage());
-				e.printStackTrace();
-				return "EmailPickerActivity: Unable to retrieve web page. URL may be invalid. " + e.getMessage();
-			}
+	@Override
+	protected String doInBackground(String... urls) {
+		Log.v(TAG, "find_or_register user " + email);
+		try {
+			return HTTPUtils.sendPost(activity, "http://semat.herokuapp.com/api/v1/users/find_or_register.json", "email=" + email);
+		} catch (IOException e) {
+			Log.v(TAG, e.getMessage());
+			return "EmailPickerActivity: Unable to retrieve web page. URL may be invalid. " + e.getMessage();
+		} catch (Exception e) {
+			Log.v(TAG, e.getMessage());
+			e.printStackTrace();
+			return "EmailPickerActivity: Unable to retrieve web page. URL may be invalid. " + e.getMessage();
 		}
+	}
 
-		@Override
-		protected void onPostExecute(String result) {
-			Log.v(TAG, "find_or_register user performing fetch callback");
-			if(result == null){
-				Toast.makeText(activity, "Cannot login - device offline", Toast.LENGTH_LONG).show();
-				super.onPostExecute(result);
-				return;
-			}
-			Log.v(TAG, result);
-			
-			if(result.equals("{\"response\":true}")) {
-				((EmailPickerActivity)activity).moveToLoginActivity();							
-			} else { //User has not confirmed account
-				((EmailPickerActivity)activity).moveToWaitForRegistrationEmailActivity();
-			}
+	@Override
+	protected void onPostExecute(String result) {
+		Log.v(TAG, "find_or_register user performing fetch callback");
+		if(result == null){
+			Toast.makeText(activity, "Cannot login - device offline", Toast.LENGTH_LONG).show();
+			super.onPostExecute(result);
+			return;
 		}
-	}	
+		Log.v(TAG, result);
+
+		if(result.equals("{\"response\":true}")) {
+			((EmailPickerActivity)activity).moveToLoginActivity();							
+		} else { //User has not confirmed account
+			((EmailPickerActivity)activity).moveToWaitForRegistrationEmailActivity();
+		}
+	}
+}	
